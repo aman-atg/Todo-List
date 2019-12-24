259f3b5e1b1ecd9d43febec485ba2c51b82b1f0d
Aman <amanansari.a2@gmail.com>
2019-12-24 14:28:53 +0530

    Some Modifications

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,28 +1,30 @@
 
-// * li completed
-$("ul").on("click","li",function(){
-     $(this).toggleClass("completed");
-});
+$(document).ready(function () {
+    // * li completed
+    $("ul").on("click","li",function(){
+        $(this).toggleClass("completed");
+    });
 
-$("ul").on("click","span",function(e){
+    $("ul").on("click","span",function(e){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     e.stopPropagation();
 
-});
+    });
 
-var inp = $("input[type='text'");
+    var inp = $("input[type='text'");
 
-inp.keypress(function(e){
-   if(e.which === 13)
-   {    
+    inp.keypress(function(e){
+    if(e.which === 13)
+    {    
         var newTask = inp.val();
         var x = $("ul").append("<li> <span> <i class='fa fa-trash'></i> </span>"+ newTask + " </li>" );
         inp.val("");
-   }
-});
+    }
+    });
 
-$(".fa-plus").click(function(){
+    $(".fa-plus").click(function(){
     $("input[type='text']").fadeToggle();
+    });    
 });
